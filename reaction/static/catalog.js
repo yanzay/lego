99418b6e90cc6ab349d0c2e6bff4983bc846d1cc
Alexey Grachov <grachov.alexey@gmail.com>
2015-10-04 00:38:03 +0300

    Selecting categories.

--- reaction/static/catalog.js
+++ reaction/static/catalog.js
@@ -1,21 +1,55 @@
-var Menu = React.createClass({
+var Shop = React.createClass({
+  getInitialState: function() {
+    return {selectedCategory: null};
+  },
+  onSelectCategory: function(categoryId) {
+    console.log(categoryId);
+    this.setState({selectedCategory: categoryId});
+  },
   render: function() {
     return (
+      <div className="row">
+        <div className="col-md-2">
+          <Menu categories={window.categories} onSelectCategory={this.onSelectCategory} />
+          <div id="menu"></div>
+        </div>
+        <div className="col-md-7">
+          <Products data={window.products} categoryId={this.state.selectedCategory} />
+          <div id="products"></div>
+        </div>
+        <div className="col-md-3">
+          <Cart items={window.items} />
+          <div id="cart"></div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Menu = React.createClass({
+  render: function() {
+    var that = this
+    var menuNodes = this.props.categories.map(function(category, index) {
+      return <MenuItem name={category.name} id={category.id} key={index} onSelectCategory={that.props.onSelectCategory} />
+    });
+    return (
       <div className="list-group row">
-        <MenuItem name="Улун" />
-        <MenuItem name="Пуэр" />
-        <MenuItem name="Зелёный чай" />
-        <MenuItem name="Чёрный чай" />
+        {menuNodes}
       </div>
     );
   }
 });
 
 var MenuItem = React.createClass({
+  handleCategory: function() {
+    this.props.onSelectCategory(this.props.id)
+  },
   render: function() {
-    return (
-      <a href="#" className="list-group-item">{this.props.name}</a>
-    );
+      return (
+        <a className="list-group-item" onClick={this.handleCategory} href="javascript:void(0);">
+          {this.props.name}
+        </a>
+      );
   }
 });
 
@@ -24,16 +58,22 @@ var Products = React.createClass({
     return {selected: false};
   },
   render: function() {
-    var nodes = this.props.data.map(function(product, index){
-      return <ProductCard
-        key={index}
-        name={product.name}
-        price={product.price}
-        img={product.img} />
+    var productNodes = [];
+    var categoryId = this.props.categoryId;
+    this.props.data.forEach(function(product, index){
+      if (product.categoryId == categoryId || categoryId == null) {
+        productNodes.push(
+          <ProductCard
+            key={index}
+            name={product.name}
+            price={product.price}
+            img={product.img} />
+        );
+      }
     });
     return (
       <div className="row">
-        {nodes}
+        {productNodes}
       </div>
     );
   }
@@ -61,15 +101,66 @@ var ProductCard = React.createClass({
   }
 });
 
-window.products = [
-  {name: "Чай не скучай", price: "120", img: "http://placehold.it/320x200"},
-  {name: "Чай не скучай", price: "120", img: "http://placehold.it/320x200"},
-  {name: "Чай не скучай", price: "120", img: "http://placehold.it/320x200"},
-  {name: "Чай не скучай", price: "120", img: "http://placehold.it/320x200"},
-  {name: "Чай не скучай", price: "120", img: "http://placehold.it/320x200"},
-  {name: "Чай не скучай", price: "120", img: "http://placehold.it/320x200"}
+var Cart = React.createClass({
+  render: function() {
+    var itemNodes = this.props.items.map(function(item, index){
+      return <CartItem
+        key={index}
+        name={item.name}
+        price={item.price}
+        count={item.count} />
+    });
+    return (
+      <div className="row">
+        <div className="panel panel-success">
+          <div className="panel-heading">Корзина</div>
+          <table className="table table-condensed table-hover">
+            <tbody>
+              {itemNodes}
+            </tbody>
+          </table>
+          <div className="panel-footer">Всего: 1000 грн.</div>
+        </div>
+        <a href="javascript:void(0);" className="btn btn-primary">Оформить заказ</a>
+      </div>
+    );
+  }
+});
+
+var CartItem = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>
+            <input type="number" value={this.props.count} style={{width: 2 + 'em'}} />
+        </td>
+        <td>{this.props.price}</td>
+        <td><button type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></td>
+      </tr>
+    );
+  }
+});
+
+window.items = [
+  {name: "Чай не скучай", price: "120", count: "1"},
+  {name: "Те Гуань Инь", price: "140", count: "2"}
 ];
 
-React.render(<Menu />, document.getElementById("menu"))
-React.render(<Products data={window.products} />, document.getElementById("products"))
+window.products = [
+  {name: "Да Хун Пао", price: "120", img: "http://placehold.it/320x200", categoryId: 1},
+  {name: "Те Гуань Инь", price: "120", img: "http://placehold.it/320x200", categoryId: 1},
+  {name: "Пуэр 1", price: "120", img: "http://placehold.it/320x200", categoryId: 2},
+  {name: "Пуэр 2", price: "120", img: "http://placehold.it/320x200", categoryId: 2},
+  {name: "Зелёный", price: "120", img: "http://placehold.it/320x200", categoryId: 3},
+  {name: "Седой Граф", price: "120", img: "http://placehold.it/320x200", categoryId: 4}
+];
 
+window.categories = [
+  {name: "Улун", id: 1},
+  {name: "Пуэр", id: 2},
+  {name: "Зелёный чай", id: 3},
+  {name: "Чёрный чай", id: 4}
+];
+
+React.render(<Shop />, document.getElementById("shop"))
